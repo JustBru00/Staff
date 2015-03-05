30d440f5e97f1b27f0e261f01f03454918a9e855
Justin Brubaker <justbru00@gmail.com>
2015-03-04 21:34:21 -0500

    Removed more unused imports.

--- src/justbru00/staff/Staff.java
+++ src/justbru00/staff/Staff.java
@@ -16,8 +16,6 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
 
 public class Staff extends JavaPlugin implements Listener {
 
